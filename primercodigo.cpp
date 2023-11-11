e71968a9c59b5f0917c4118ac396602261848061
Ascastillo98 <ascastillo1998@gmail.com>
2023-11-11 13:39:59 +0100

    nuevas carpetas y actu

--- primercodigo.cpp
+++ primercodigo.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     cout << "hola mundo" << endl;
     for(int i = 0; i <10;i++){
-        cout << "Hola!" << endl;
+        cout << "Hola a todos!" << endl;
     }
 }
 
